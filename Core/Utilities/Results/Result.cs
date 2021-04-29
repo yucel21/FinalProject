3d79ccad26eedcd1e0b3ecfdedbccd9a7f3566a4
yuceL <ycL@DESKTOP-PG4QN3E>
2021-04-29 03:20:21 +0300

    Add project files.

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //2 parametre gönderen biri yukardaki çalışıyor
+        //this ile aşağıdaki metodu da kapsadığı için aşağıdaki kodu da çalıştırıyoruz
+        public Result(bool success, string message):this(success)
+        {
+            Message = message;
+        }
+
+        public Result(bool success)
+        {            
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
